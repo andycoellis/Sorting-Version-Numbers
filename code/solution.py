5e1d00f43eb440f5695e99698486cae20fdde7a4
Andy Ellis <andy_ellis@mac.com>
2021-01-03 16:23:22 +1000

    ADDED Version class functionality

--- code/solution.py
+++ code/solution.py
@@ -11,15 +11,41 @@ def solution(l):
 		if not pattern.match(item):
 			raise ValueError("An item does not fill the language format")
 
+	# Convert list of strings into a list of ints
+	l = tokenizer(l)
+
+	# Sorted list of version numbers
+	l = mergeSort(l)
+
 	return l
 
 
-def tokenizer(w):
+def mergeSort(l):
+	""" 
+	Implemented version of foundational sorting algorithm merge sort,
+	algorithm based off 'geeks for geeks' website=https://www.geeksforgeeks.org/merge-sort/
+
+	"""
+	# if len(l) > 1:
+	# 	mid_point = len(l)//2
+		
+	# 	left_half = l[:mid_point]
+	# 	right_half = l[mid_point:]
+
+	# 	mergeSort(left_half)
+	# 	mergeSort(right_half)
+
+	# 	i = j = k = 0
+
+
+
+
+def tokenizer(l):
 	"""
 	Returns an ordered array of integers converterd from serialised strings
 	"""
 	solution = []
-	for word in w:
+	for word in l:
 		number = word.split('.')
 		
 		for i in range(0, len(number)):
@@ -28,3 +54,103 @@ def tokenizer(w):
 		solution.append(number)
 
 	return solution
+
+
+def convert_to_list(l):
+	""" Returns a blanket three digit version number of given argument """
+	version_list = [0] * 3
+
+	for i in range(0, len(l)):
+		version_list[i] = l[i]
+
+	return version_list
+
+
+def label_list(v, o):
+	"""
+	Returns a list of labels that signify the relationship between two lists:
+	
+	E: Equals
+	L: Less Than
+	G: Greate Than
+	"""
+	check = [None] * 3
+	for i in range(0, len(v)):
+
+		if v[i] == o[i]:
+			check[i] = 'E'
+		elif v[i] < o[i]:
+			check[i] = 'L'
+		else:
+			check[i] = 'G'
+
+	return check
+
+
+class Version(list):
+	"""
+	This is a representation of a version number, this allows for 
+	operator overriding functionality that allows cleaner code
+	"""
+	def __init__(self, v):
+		self.v = []
+		self.length = len(v)
+
+		for item in v:
+			self.v.append(item)
+
+
+	def __eq__(self, o):
+		check = self.check_equivalence(o)
+
+		return True if check == 'E' else False
+
+	
+	def __lt__(self, o):
+		check = self.check_equivalence(o)
+
+		return True if check == 'L' else False
+		
+
+	def __gt__(self, o):
+		check = self.check_equivalence(o)
+
+		return True if check == 'G' else False
+
+
+	def __str__(self):
+		response = None
+
+		for i in range(0, len(self.v)):
+			if i == 0:
+				response = str(self.v[i])
+			else:
+				response = response + '.' + str(self.v[i])
+
+		return response
+
+
+	def __len__(self):
+		return len(self.v)
+
+
+	def __getitem__(self, i):
+		return self.v[i]
+
+	def check_equivalence(self, o):
+		""" Returns the appropriate label {'E', 'L', 'G'} of conditional property """
+		check = label_list(convert_to_list(self.v), convert_to_list(o))
+
+		count = 0
+		for item in check:
+			if item == 'E':
+				count += 1
+
+		response = 'E' if count == 3 else None
+
+		if response is None:
+			for item in check:
+				if response is None and item != 'E':
+					response = item
+
+		return response
